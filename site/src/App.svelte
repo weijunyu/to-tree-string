9b9b1cb737f606605c16ecca6e2592bd7f0f2379
junyu <junyu@example.com>
2022-11-25 13:09:48 +0800

    Basic app with yaml input

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
-  import Counter from "./lib/Counter.svelte";
+  import { toTreeString } from "@jywei/to-tree-string";
+  import { parse } from "yaml";
+
+  let yamlInput = `example:
+  - image1.jpg`;
+
+  let outputString = "";
+
+  function generate() {
+    outputString = toTreeString(parse(yamlInput));
+    console.log(outputString);
+  }
 </script>
 
 <main>
   <h1>Demo</h1>
-  <Counter />
+
+  <textarea bind:value={yamlInput} />
+
+  <button on:click={generate}>Generate</button>
+
+  <pre>{outputString}</pre>
 </main>
 
 <style>
